f7f3c30020052dcfd4572c1b1d3760f822ab29dc
virendra kaira <virendrakaira60@gmail.com>
2025-01-21 20:15:06 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,12 +19,15 @@ const Hero = () => {
          <div>
 
 <Link to="work" smooth={true} duration={500} >
+
 <button className='text-white  group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 text-2xl sm:text-3xl '>View Work  
+          
            <span className='group-hover:rotate-90 duration-300'>
           <HiArrowNarrowRight className='ml-3 ' />
           </span>
+
             </button> 
-        </Link>
+            </Link>
 
           
             
