86da860ab6d03387885df633229ac8da3e0ae9b4
virendra kaira <virendrakaira60@gmail.com>
2025-01-21 16:24:34 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,87 +3,87 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo1.png';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
   return (
     <div className="fixed w-full h-[48px] flex justify-between items-center px-4 bg-[#25559e] text-gray-100">
-          <div>
-              <img  src={Logo} alt="logo" style={{width:'50px' }} />
-          </div>
-          {/* menu */}
-              <ul className=' hidden md:flex  text-[20px]'>
+      <div>
+        <img src={Logo} alt="logo" style={{ width: '50px' }} />
+      </div>
+      {/* menu */}
+      <ul className=' hidden md:flex  text-[20px]'>
         <li className='hover:border-b-2 border-pink-500'>
           <Link to="Hero" smooth={true} duration={500} >
-             Home
-        </Link>
+            Home
+          </Link>
         </li>
-        
+
         <li className='hover:border-b-2 border-pink-500'>
-          <Link to="about"  smooth={true} duration={500} >
-             About
-        </Link>
+          <Link to="about" smooth={true} duration={500} >
+            About
+          </Link>
         </li>
         <li className='hover:border-b-2 border-pink-500'>
-        <Link to="skills" smooth={true} duration={500} >
-             Skills
-        </Link>
-          </li>
+          <Link to="skills" smooth={true} duration={500} >
+            Skills
+          </Link>
+        </li>
         <li className='hover:border-b-2 border-pink-500 '>
-        <Link to="work" smooth={true} duration={500} >
-             Work
-        </Link>
-                  </li>
+          <Link to="work" smooth={true} duration={500} >
+            Work
+          </Link>
+        </li>
         <li className='hover:border-b-2 border-pink-500 '>
-        <Link to="contact" smooth={true} duration={500} >
-             Contact
-        </Link>
-                  </li>
-              
-              </ul>
+          <Link to="contact" smooth={true} duration={500} >
+            Contact
+          </Link>
+        </li>
 
-          {/* menue icon */}
-          <div onClick={handleClick}  className='md:hidden  z-10'>
-            {!nav?   <FaBars/>:<FaTimes/>}
-          </div>
+      </ul>
 
-          {/* Mobile menu */}
-      <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center' }>
+      {/* menue icon */}
+      <div onClick={handleClick} className='md:hidden  z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         <li className="py-6 text-3xl">
           <Link onClick={handleClick} to="Hero" smooth={true} duration={500} >
-             Home
-        </Link></li>
+            Home
+          </Link></li>
         <li className="py-6 text-3xl">
-        <Link onClick={handleClick} to="about"  smooth={true} duration={500} >
-             About
-        </Link>
-                  </li>
+          <Link onClick={handleClick} to="about" smooth={true} duration={500} >
+            About
+          </Link>
+        </li>
         <li className="py-6 text-3xl">
-        <Link onClick={handleClick} to="skills" smooth={true} duration={500} >
-             Skills
-        </Link>
-                  </li>
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500} >
+            Skills
+          </Link>
+        </li>
         <li className="py-6 text-3xl">
-        <Link onClick={handleClick} to="work" smooth={true} duration={500} >
-             Work
-        </Link>
-                  </li>
+          <Link onClick={handleClick} to="work" smooth={true} duration={500} >
+            Work
+          </Link>
+        </li>
         <li className="py-6 text-3xl">
-        <Link onClick={handleClick} to="contact" smooth={true} duration={500} >
-             Contact
-        </Link>
-                  </li>
-          </ul>
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500} >
+            Contact
+          </Link>
+        </li>
+      </ul>
 
-          {/* Social icons */}
+      {/* Social icons */}
       <div className='hidden xl:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-8px] duration-300 bg-blue-600'>
             <a className='flex justify-between items-center w-full text-gray-300 '
               href="https://www.linkedin.com/in/virendra-kaira-a070332bb/">
-              Linkedin<FaLinkedin size={40}  />
+              Linkedin<FaLinkedin size={40} />
             </a>
           </li>
           <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-10px] duration-300 bg-[#333333]'>
@@ -101,11 +101,11 @@ const Navbar = () => {
           <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-10px] duration-300 bg-[#24557e]'>
             <a className='flex justify-between items-center w-full text-gray-300 '
               href="https://www.canva.com/design/DAGPnArTNQY/x5O--3KXyhF_Haa3vqSaPw/edit?utm_content=DAGPnArTNQY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
-            Resume< BsFillPersonLinesFill size={40} />
+              Resume< BsFillPersonLinesFill size={40} />
             </a>
           </li>
         </ul>
-          </div>
+      </div>
     </div>
   )
 }
