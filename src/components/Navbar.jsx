a00cb3b214de0cd3d6db50b63b0edcca5d6f87c0
virendra kaira <virendrakaira60@gmail.com>
2025-01-21 17:38:46 +0530

    navbarchange

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,30 +51,61 @@ const Navbar = () => {
 
       {/* Mobile menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className="py-6 text-3xl">
+        <li className="py-6 text-3xl hover:border-b-[2px] border-pink-500">
           <Link onClick={handleClick} to="Hero" smooth={true} duration={500} >
             Home
           </Link></li>
-        <li className="py-6 text-3xl">
+        <li className="py-6 text-3xl hover:border-b-[2px] border-pink-500 ">
           <Link onClick={handleClick} to="about" smooth={true} duration={500} >
             About
           </Link>
         </li>
-        <li className="py-6 text-3xl">
+        <li className="py-6 text-3xl hover:border-b-[2px] border-pink-500 ">
           <Link onClick={handleClick} to="skills" smooth={true} duration={500} >
             Skills
           </Link>
         </li>
-        <li className="py-6 text-3xl">
+        <li className="py-6 text-3xl hover:border-b-[2px] border-pink-500 ">
           <Link onClick={handleClick} to="work" smooth={true} duration={500} >
             Work
           </Link>
         </li>
-        <li className="py-6 text-3xl">
+        <li className="py-6 text-3xl hover:border-b-[2px] border-pink-500 ">
           <Link onClick={handleClick} to="contact" smooth={true} duration={500} >
             Contact
           </Link>
         </li>
+          <ul className=' flex flex-row'>
+        <li className='w-[60px] h-[50px] flex justify-between items-center  duration-300 bg-blue-600'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="https://www.linkedin.com/in/virendra-kaira-a070332bb/">
+              <FaLinkedin size={50} />
+            </a>
+          </li>
+
+          <li className='w-[60px] h-[50px] flex mx-4 justify-between items-center  duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="https://github.com/12virendra">
+              <FaGithub size={50} />
+            </a>
+          </li>
+
+          <li className='w-[60px] h-[50px] flex justify-between mr-4 items-center   duration-300 bg-orange-600'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="mailto:virendrakaira60@gmail.com ">
+              < HiOutlineMail size={50} />
+            </a>
+          </li>
+
+          <li className='w-[60px] h-[50px] flex justify-between items-center    duration-300 bg-[#24557e]'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="https://www.canva.com/design/DAGPnArTNQY/x5O--3KXyhF_Haa3vqSaPw/edit?utm_content=DAGPnArTNQY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
+              < BsFillPersonLinesFill size={50} />
+            </a>
+          </li>
+
+
+          </ul>
       </ul>
 
       {/* Social icons */}
@@ -86,19 +117,19 @@ const Navbar = () => {
               Linkedin<FaLinkedin size={40} />
             </a>
           </li>
-          <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+          <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-8px]  duration-300 bg-[#333333]'>
             <a className='flex justify-between items-center w-full text-gray-300 '
               href="https://github.com/12virendra">
               GitHub<FaGithub size={40} />
             </a>
           </li>
-          <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-10px] duration-300 bg-orange-600'>
+          <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-8px]  duration-300 bg-orange-600'>
             <a className='flex justify-between items-center w-full text-gray-300 '
               href="mailto:virendrakaira60@gmail.com ">
               Email< HiOutlineMail size={40} />
             </a>
           </li>
-          <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-10px] duration-300 bg-[#24557e]'>
+          <li className='w-[220px] h-[60px] flex justify-between items-center ml-[-150px] hover:ml-[-8px]  duration-300 bg-[#24557e]'>
             <a className='flex justify-between items-center w-full text-gray-300 '
               href="https://www.canva.com/design/DAGPnArTNQY/x5O--3KXyhF_Haa3vqSaPw/edit?utm_content=DAGPnArTNQY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
               Resume< BsFillPersonLinesFill size={40} />
@@ -106,6 +137,37 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+
+{/* More icons */}
+
+{/* <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+          <li className='w-[30px] h-[30px] flex justify-between items-center ml-[-150px]  duration-300 bg-blue-600'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="https://www.linkedin.com/in/virendra-kaira-a070332bb/">
+              <FaLinkedin size={20} />
+            </a>
+          </li>
+          <li className='w-[30px] h-[30px] flex justify-between items-center ml-[50px]  duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="https://github.com/12virendra">
+              <FaGithub size={20} />
+            </a>
+          </li>
+          <li className='w-[30px] h-[30px] flex justify-between items-center ml-[50px]  duration-300 bg-orange-600'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="mailto:virendrakaira60@gmail.com ">
+              < HiOutlineMail size={20} />
+            </a>
+          </li>
+          <li className='w-[30px] h-[30px] flex justify-between items-center ml-[50px]  duration-300 bg-[#24557e]'>
+            <a className='flex justify-between items-center w-full text-gray-300 '
+              href="https://www.canva.com/design/DAGPnArTNQY/x5O--3KXyhF_Haa3vqSaPw/edit?utm_content=DAGPnArTNQY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
+              < BsFillPersonLinesFill size={20} />
+            </a>
+          </li>
+        </ul> */}
+
+
     </div>
   )
 }
