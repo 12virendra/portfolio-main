071b3fe6e0dcd5dfba1af17407943182b3406987
virendra kaira <virendrakaira60@gmail.com>
2025-02-15 00:33:41 +0530

    new changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,6 +6,7 @@ import todoimg from '../assets/TODOIMG.png';
 import rockimg from '../assets/rockgame.png';
 import tictacimg from '../assets/tictac.png';
 import passwordimg from '../assets/password.png'
+import currency from '../assets/currency.png'
 
 const Work = () => {
     return (
@@ -16,51 +17,57 @@ const Work = () => {
                     <p className='py-6 max-sm:text-[25px]'>Check out some of my recent work .</p>
                 </div>
 
-                {/* make it responsive when you will see it after wards */}
+                
 
                 <div className='flex  w-full '>
                     <div className='w-full grid grid-cols-1 sm:grid-cols-2 gap-4 text-center py-8 '>
                         <div className='  w-80     '>
                             <img className='w-60  h-60 mx-9 hover:scale-110 duration-500' src={netflix} alt="Netflix" />
                             <a className='  cursor-pointer hover:border-b-4 duration-150  text-gray-300 border-pink-300 text-2xl ' href='https://github.com/12virendra/Netflix-clone' target='_main'>Netflix clone </a>
-                            <a className=' mx-11 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/Netflix-clone/">LIVE</a>
+                            <a className=' mx-11 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://timely-wisp-f72664.netlify.app/">LIVE</a>
 
                         </div>
                         <div className='  w-80 max-sm:w-[294px] '>
                             <img className='w-60  h-60 mx-11 hover:scale-110 duration-500' src={amazon} alt="amazon" />
                             <a className=' mx-5 cursor-pointer hover:border-b-4 duration-150 text-gray-300 border-pink-300 text-2xl ' href='https://github.com/12virendra/Amazon-clone' target='_main'>Amazon clone </a>
-                            <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/Amazon-clone/">LIVE</a>
+                            <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://stalwart-ganache-d930fc.netlify.app/">LIVE</a>
 
                         </div>
                         <div className='  w-80  max-sm:w-[299px]   '>
                             <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={spotify} alt="spotify" />
                             <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-gray-300 border-pink-300  text-2xl ' href='https://github.com/12virendra/spotify-clone' target='_main'>Spotify clone </a>
-                            <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/spotify-clone/">LIVE</a>
+                            <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://sweet-sable-858bd7.netlify.app/">LIVE</a>
                         </div> 
-                        {/* jkjkjk */}
+                        
 
                         <div className='  w-80  max-sm:w-[299px]   '>
-                            <img className='w-[230px]  h-[230px] mx-14 hover:scale-110 duration-500' src={todoimg} alt="spotify" />
+                            <img className='w-[230px]  h-[230px] mx-14 hover:scale-110 duration-500' src={todoimg} alt="Todo " />
                             <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-gray-300 border-pink-300  text-2xl ' href='https://github.com/12virendra/Todo-List ' target='_main'>Todo App </a>
-                            <a className=' mx-11 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/Todo-List/">LIVE</a>
+                            <a className=' mx-11 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://todo-list-indol-theta-77.vercel.app/">LIVE</a>
                         </div>
 
                         <div className='  w-80  max-sm:w-[299px]   '>
-                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={rockimg} alt="spotify" />
+                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={rockimg} alt="Rock paper scissor" />
                             <a className=' mx-0 cursor-pointer hover:border-b-4 duration-500 text-gray-300 border-pink-300  text-2xl ' href='https://github.com/12virendra/rock-paper-game' target='_main'>Rock-Paper- Scissor</a>
-                            <a className=' ml-10 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/rock-paper-game/">LIVE</a>
+                            <a className=' ml-10 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://harmonious-panda-34a0f7.netlify.app/">LIVE</a>
                         </div>
 
                         <div className='  w-80  max-sm:w-[299px]   '>
-                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={tictacimg} alt="spotify" />
+                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={tictacimg} alt="tic tac toe" />
                             <a className=' mx-5 cursor-pointer hover:border-b-4 duration-500 text-gray-300 border-pink-300  text-2xl ' href=' https://github.com/12virendra/Tic-tac-toe' target='_main'>Tic-tac-toe </a>
-                            <a className=' mx-11 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/Tic-tac-toe/">LIVE</a>
+                            <a className=' mx-11 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://chic-torte-d639d8.netlify.app/">LIVE</a>
                         </div>
 
                         <div className='  w-80  max-sm:w-[299px]   '>
-                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={passwordimg} alt="spotify" />
+                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={passwordimg} alt="password" />
                             <a className=' mx-0 cursor-pointer hover:border-b-4 duration-500 text-gray-300 border-pink-300  text-2xl ' href=' https://github.com/12virendra/Password-generator' target='_main'>Password-Generator-App </a>
-                            <a className=' ml-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://12virendra.github.io/Password-generator/" target=''>LIVE</a>
+                            <a className=' ml-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://password-generator-opal-kappa.vercel.app/" target=''>LIVE</a>
+                        </div>
+
+                        <div className='  w-80  max-sm:w-[299px]   '>
+                            <img className='w-[235px]  h-60 mx-14 hover:scale-110 duration-500' src={currency} alt="currency" />
+                            <a className=' mx-0 cursor-pointer hover:border-b-4 duration-500 text-gray-300 border-pink-300  text-2xl ' href='https://github.com/12virendra/Currency-Converter' target='_main'>Currency Converter </a>
+                            <a className=' ml-5 cursor-pointer hover:border-b-4 duration-500 text-pink-300 border-pink-300  text-lg ' href="https://currency-converter-nine-orpin.vercel.app/" target=''>LIVE</a>
                         </div>
 
                     </div>
